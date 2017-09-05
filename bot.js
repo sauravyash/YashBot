a5ac154bbfa0ace0313bd67930a367d1e78c7b1e
sauravyash <yash_agasti@hotmail.com>
2017-09-05 20:43:37 +1000

    Change to bot

--- bot.js
+++ bot.js
@@ -1,3 +1,18 @@
+if (fs.existsSync('./config.json')) {
+    var config = require("./config.json");
+    var discordKey = config.discordKey;
+    var witKey = config.witKey;
+    var wolframKey = config.wolframKey;
+    var giphyKey = config.giphyKey;
+    var youtubeKey = config.youtubeKey;
+}
+else {
+  var discordKey = process.env.discordKey;
+  var witKey = process.env.witKey;
+  var wolframKey = process.env.wolframKey;
+  var giphyKey = process.env.giphyKey;
+  var youtubeKey = process.env.youtubeKey;
+}
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
